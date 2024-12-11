b2e7588ce3d00ce1dbc2cd1566aead519464f43b
Your Name <you@example.com>
2024-12-11 23:33:05 +0900

    to be continued

--- router/parkingRouter.js
+++ router/parkingRouter.js
@@ -6,12 +6,17 @@ router.get('/aispace', (req ,res)=>{
     parking.aispace(req, res);
 })
 
-router.post('/receiver', (req, res)=>{
-    parking.receiver(req,res)
+router.post('/AIreceiver', (req, res)=>{
+    parking.AI_receiver(req,res)
 })
 
 router.get('/center',(req, res)=>{
     parking.center(req, res)
+})
+
+
+router.post('/centerreceiver', (req, res)=>{
+    parking.center_receiver(req,res)
 })
 
 module.exports= router;
